ec85d666f6ce03c59fa041c2af120ac90df6702f
Emmanuel-9 <oregadaniel181@gmail.com>
2020-06-15 05:56:48 +0300

    Import VotecountDirective && add describe function.

---
+++ src/app/votecount.directive.spec.ts
@@ -0,0 +1,8 @@
+import { VotecountDirective} from './votecount.directive';
+
+describe('VotecountDirective', () => {
+  it('should create an instance', () => {
+    const directive = new VotecountDirective();
+    expect(directive).toBeTruthy();
+  });
+});
