9d9f3f615b7753449af32de57c21eaae471d59a8
Emmanuel-9 <oregadaniel181@gmail.com>
2020-06-15 05:46:41 +0300

    Add the components,pipe and directive to the declarations && FormsModule to imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesdetailsComponent } from './quotesdetails/quotesdetails.component';
+import { QuotesformComponent } from './quotesform/quotesform.component';
+import { DateCountPipe } from './date-count.pipe';
+import { VotecountDirective } from './votecount.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesdetailsComponent,
+    QuotesformComponent,
+    DateCountPipe,
+    VotecountDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
